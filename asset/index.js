516422478871e3dc5df70e3b793362974a3310a3
wurui <wurui.wr@gmail.com>
2018-11-27 18:16:27 +0800

    fix bug

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,15 @@
 define([],function(){
   return {
     init:function($mod){
-
+    	var ts=new Date();
+    	$(window).on('focus pageshow',function(){
+    		var now=new Date();
+    		if(now-ts>2000){
+    			ts=now;
+    			$mod.OXRefresh()
+    		}
+    		
+    	})
     }
   }
 })
