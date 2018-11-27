6005806a63e84f48e8b7620d2f353f98a0a6b50d
wurui <wurui.wr@gmail.com>
2018-11-27 18:43:15 +0800

    fix

--- asset/index.js
+++ asset/index.js
@@ -3,6 +3,9 @@ define([],function(){
     init:function($mod){
     	var ts=new Date();
     	
+    	$('#Refresh').on('click',function(){
+    		$mod.OXRefresh()
+    	})
     	window.addEventListener('pageshow',function(){
     		
     		var now=new Date();
